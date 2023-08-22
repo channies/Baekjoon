d980ffe9a1f27dfe5491c37532629c2b63a2492e
channies <81520254+channies@users.noreply.github.com>
2023-08-22 17:53:02 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 672 ms, Memory: 65176 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,26 @@
 import sys
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-graph = [[] for _ in range(n+1)]
-
-for i in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-    
+cnt = 0
 visited = [False] * (n+1)
-def dfs(x):
-    visited[x] = True
-    for i in graph[x]:
+arr = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+    
+def dfs(s):
+    visited[s] = True
+    for i in arr[s]:
         if not visited[i]:
             dfs(i)
-
-cnt = 0
+            
 for i in range(1, n+1):
     if not visited[i]:
         dfs(i)
         cnt += 1
 print(cnt)
-    
