25eb4f399ce367a7695dbb83775b72e5d4b0832f
channies <81520254+channies@users.noreply.github.com>
2023-09-06 17:00:06 +0900

    [Silver I] Title: 그림, Time: 364 ms, Memory: 44896 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,5 +1,7 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 6)
+
+from collections import deque
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
@@ -7,27 +9,33 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 ans = []
+a = 0
             
-def dfs(x,y):
-    global cnt
-    arr[x][y] = 0
-    cnt += 1
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
-            dfs(nx,ny)
-   
-    
+def bfs(xs,ys):
+    q = deque()
+    q.append((xs,ys))
+    cnt = 1
+    arr[xs][ys] = 0    
+    while q:
+        x, y = q.pop()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
+                q.append((nx,ny))
+                arr[nx][ny] = 0
+                cnt += 1
+    return cnt
+
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 1:
-            cnt = 0
-            dfs(i,j)
+            cnt = bfs(i,j)
+            a += 1
             ans.append(cnt)
             
 if ans:
-    print(len(ans))
+    print(a)
     print(max(ans))
 else:
     print(0)
