58c49fd393a4b67287c8120a5f8d5c377643452a
channies <81520254+channies@users.noreply.github.com>
2023-09-07 15:21:00 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -2,20 +2,21 @@ n, m = map(int, input().split())
 x, y, d = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [-1, 0, 1, 0]
+dx =[-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def dfs(x, y, d):
+def dfs(x,y,d):
     global cnt
     if arr[x][y] == 0:
         arr[x][y] = 2
-        cnt += 1
+        cnt += 1    
     for _ in range(4):
         nd = (d+3) % 4
         nx = x + dx[nd]
         ny = y + dy[nd]
+        
         if arr[nx][ny] == 0:
-            dfs(nx, ny, nd)
+            dfs(nx,ny,nd)
             return
         d = nd
     nd = (d+2) % 4
@@ -23,9 +24,10 @@ def dfs(x, y, d):
     ny = y + dy[nd]
     if arr[nx][ny] == 1:
         return
-    dfs(nx, ny, d)
-    
+    dfs(nx,ny,d)
 cnt = 0
-dfs(x, y, d)
+  
+dfs(x,y,d)
 print(cnt)
+            
         
