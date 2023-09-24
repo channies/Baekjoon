e46e0355fd27942b319ef3e17102fe3b743b4c55
channies <81520254+channies@users.noreply.github.com>
2023-09-24 23:44:20 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -3,11 +3,11 @@ coins = []
 for _ in range(N):
     coins.append(int(input()))
 coins.sort(reverse=True)
-cnt = 0
+count = 0
 for coin in coins:
     if coin <= K:
-        cnt += K // coin
+        count += K // coin
         K %= coin
         if K == 0:
             break
-print(cnt)
+print(count)
