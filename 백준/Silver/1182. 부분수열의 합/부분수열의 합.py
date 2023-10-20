5f3e2f00336060cb15b2725ba9a8f6cc18a76ee3
channies <81520254+channies@users.noreply.github.com>
2023-10-20 20:07:11 +0900

    [Silver II] Title: 부분수열의 합, Time: 504 ms, Memory: 80888 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+
+for i in range(1, n+1):
+    comb = list(combinations(arr, i))
+    for k in comb:
+        if sum(k) == s:
+            cnt += 1
+            
+print(cnt)
