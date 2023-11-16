caf4a2219f8baad381e1c1b78c99fbb5a634c523
channies <81520254+channies@users.noreply.github.com>
2023-11-16 09:41:02 +0900

    [Silver III] Title: N과 M (1), Time: 116 ms, Memory: 35556 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -4,8 +4,5 @@ n, k = map(int, input().split())
 arr = [i+1 for i in range(n)]
 ans = list(permutations(arr, k))
 
-for i in range(k):
-    ans.sort(key = lambda x: x[k-1-i])
-    
 for answer in ans:
     print(' '.join(map(str, answer)))
